ac242bf663f76eb68002846783a1cc6b4d47c6c7
jrahappy <william.bh.lee@gmail.com>
2024-02-01 02:52:51 -0500

    project_folder removed

---
+++ 2ndops.cmd
@@ -0,0 +1,4 @@
+@echo off
+cd D:/2ndOps
+set DJANGO_SETTINGS_MODULE=config.settings.local
+D:/2ndOps/2ndOps/scripts/activate
---
+++ config/settings/prod.py
@@ -0,0 +1,4 @@
+from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = ["52.200.129.44", "ddsimage.com"]  # Add your IP here
